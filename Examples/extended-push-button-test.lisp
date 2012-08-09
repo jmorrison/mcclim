d81b61309600a10899a91ff667ce3ebddf641c0e
Mariano Montone <marianomontone@gmail.com>
2012-08-09 11:32:34 -0300

    mcclim: Accept and cancel button making functions

--- Examples/extended-push-button-test.lisp
+++ Examples/extended-push-button-test.lisp
@@ -2,25 +2,35 @@
 
 (defparameter *icons-directory* (asdf::system-relative-pathname :mcclim #p"Extensions/icons/"))
 
+(defun make-accept-button (&rest args)
+  (with-look-and-feel-realization ((frame-manager *application-frame*) *application-frame*)
+    (apply #'make-pane 'push-button
+           (concatenate 'cons args
+                        (list :icon (make-pattern-from-bitmap-file
+                                     (merge-pathnames *icons-directory* #p"dialog-apply.xpm")
+                                     :format :xpm :port nil))))))
+
+(defun make-cancel-button (&rest args)
+  (with-look-and-feel-realization ((frame-manager *application-frame*) *application-frame*)
+    (apply #'make-pane 'push-button
+           (concatenate 'cons args
+                        (list :icon (make-pattern-from-bitmap-file
+                                     (merge-pathnames *icons-directory* #p"dialog-cancel-3.xpm")
+                                     :format :xpm :port nil))))))
+
+
 (define-application-frame extended-push-button-test ()
   ()
   (:panes
-   (accept-button :push-button
-                  :icon (make-pattern-from-bitmap-file
-                         (merge-pathnames *icons-directory* #p"dialog-apply.xpm")
-                         :format :xpm :port nil)
-                  :icon-position :left
-                  :label "Accept"
-                  :x-spacing 5
-                  :y-spacing 5)
-   (cancel-button :push-button
-                  :icon (make-pattern-from-bitmap-file
-                         (merge-pathnames *icons-directory* #p"dialog-cancel-3.xpm")
-                         :format :xpm :port nil)
-                  :icon-position :right
-                  :label "Cancel"
-                  :x-spacing 5
-                  :y-spacing 5))
+   (accept-button (make-accept-button
+                   :icon-position :left
+                   :label "Accept"
+                   :x-spacing 5
+                   :y-spacing 5))
+   (cancel-button (make-cancel-button
+                   :label "Cancel"
+                   :x-spacing 5
+                   :y-spacing 5)))
   (:layouts
    (default
        (vertically ()
